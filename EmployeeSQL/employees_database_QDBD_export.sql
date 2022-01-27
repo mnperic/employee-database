d03955a23684b2131d8171446a2e94dd04cc5091
mnperic <peric.mino@gmail.com>
2022-01-27 13:59:29 +1100

    Update employees_database_QDBD_export.sql

--- EmployeeSQL/employees_database_QDBD_export.sql
+++ EmployeeSQL/employees_database_QDBD_export.sql
@@ -1,13 +1,14 @@
-﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
--- Link to schema: https://app.quickdatabasediagrams.com/#/d/DRULMA
--- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+﻿-- Exported from QuickDBD
+-- Drop tables if they exist
+DROP TABLE IF EXISTS departments;
+DROP TABLE IF EXISTS dept_emp;
+DROP TABLE IF EXISTS dept_manager;
+DROP TABLE IF EXISTS employees;
+DROP TABLE IF EXISTS salaries;
+DROP TABLE IF EXISTS titles;
 
--- DROP TABLE IF EXISTS departments;
--- DROP TABLE IF EXISTS dept_emp;
--- DROP TABLE IF EXISTS dept_manager;
--- DROP TABLE IF EXISTS employees;
--- DROP TABLE IF EXISTS salaries;
--- DROP TABLE IF EXISTS titles;
+-- Create tables
+-- Import .csv files
 
 CREATE TABLE "departments" (
     "dept_no" VARCHAR(50)   NULL,
@@ -68,3 +69,10 @@ REFERENCES "salaries" ("emp_no");
 ALTER TABLE "employees" ADD CONSTRAINT "fk_employees_emp_title_id" FOREIGN KEY("emp_title_id")
 REFERENCES "titles" ("titles_id");
 
+-- Query * FROM each table to confirm data
+SELECT * FROM departments;
+SELECT * FROM dept_emp;
+SELECT * FROM dept_manager;
+SELECT * FROM employees;
+SELECT * FROM salaries;
+SELECT * FROM titles;
